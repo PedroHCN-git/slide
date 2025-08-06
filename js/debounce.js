c4d1843026b1911c23432a00c244873ed5e42311
PedroHCN <pedro.hcorradini05@gmail.com>
2025-08-06 20:39:50 -0300

    first commit

---
+++ js/debounce.js
@@ -0,0 +1,14 @@
+export default function debounce(callBack, delay){
+  let timer;
+
+  return (...args) => {
+    if (timer){
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      callBack(...args);
+      timer = null;
+    }, delay);
+  };
+}
