6909cccf3ef305a281e73a532537cb4df33d2ca4
PedroHCN <pedro.hcorradini05@gmail.com>
2025-08-08 21:42:29 -0300

    slide funcional

--- js/slide.js
+++ js/slide.js
@@ -1,21 +1,34 @@
 export default class Slide{
   constructor(slideWrapper, slide){
-    debugger
     this.wrapper = document.querySelector(slideWrapper);
     this.slide = document.querySelector(slide);
+    this.distance = {finalPosition: 0, startX: 0, movement: 0}
+  }
+
+  moveSlide(distanceX){
+    this.distance.movePosition = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX){
+    this.distance.movement = (this.distance.startX - clientX) * 1.6;
+    console.log(this.distance.finalPosition - this.distance.movement);
+    return this.distance.finalPosition - this.distance.movement;
   }
 
   onStart(event){
     event.preventDefault();
+    this.distance.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   onMove(event){
-   
+    this.moveSlide(this.updatePosition(event.clientX))
   }
 
   onEnd(){
     this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.distance.finalPosition = this.distance.movePosition;
   }
 
   binds(){
